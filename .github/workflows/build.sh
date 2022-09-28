ce3b60329ebf13f460ef0c7e8fab46265ad26338
ZiyangXiao <ziyang.xiao@intel.com>
2022-09-28 14:44:56 +0800

    fix build proxyv2 image step in CI (#7)
    Signed-off-by: Xiao, Ziyang <ziyang.xiao@intel.com>
    
    Signed-off-by: Xiao, Ziyang <ziyang.xiao@intel.com>

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -3,32 +3,28 @@
 # Usage:
 # ./.github/workflows/build.sh 
 
-# this script will modified upper directory istio-proxy repo
-# if self-used should attention that whether the upper directory have its own developing istio.proxy
 #first build should give permission to docker volumes
 #sudo chmod -R 777 /var/lib/docker/volumes
 
 set -e
 
 UPDATE_BRANCH=${UPDATE_BRANCH:-"master"}
-cd ..
 rm -rf istio-proxy
 git clone -b ${UPDATE_BRANCH} https://github.com/intel/istio-proxy.git
-cp -rf istio/ istio-proxy/ 
-cd istio-proxy
 
+pushd istio-proxy
 # Update to release branch as intel/envoy has updated.
 ./scripts/update_envoy.sh
 BUILD_WITH_CONTAINER=1 make build_envoy 
 BUILD_WITH_CONTAINER=1 make exportcache
+popd
 
-cd istio
+# SGX_BUILD_CONTAINER is not needed anymore.
+unset IMG
 # export env
 TAG=${TAG:-"pre-build"}
 make build
 # replace upstream envoy with local envoy in build proxyv2 image
-cd  ..
-cp -rf out/linux_amd64/envoy istio/out/linux_amd64/release/envoy
+cp -rf istio-proxy/out/linux_amd64/envoy out/linux_amd64/release/envoy
 # build proxyv2 image
-cd istio
 make docker.proxyv2
