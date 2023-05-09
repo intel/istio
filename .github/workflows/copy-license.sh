1b42f708b7b9349a40edbe53baa562471bbd1357
Xin Huang <xin1.huang@intel.com>
2023-05-09 10:26:39 +0800

    Collect licenses in licenses and intel/licenses directories then copy them into a text file. (#56)
    * Add intel dependency licenses. (#55)
    
    * Collect licenses in licenses and intel/licenses directories then copy them into a text file
    
    * Add License section in README.md
    
    * Add LICENSES.txt in istioctl image. (#58)
    
    Signed-off-by: Huang Xin <xin1.huang@intel.com>

---
+++ .github/workflows/copy-license.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+# Usage:
+# ./.github/workflows/copy-license.sh
+
+# collect licenses in licenses and intel/licenses directories then copy them into a text file
+
+set -e
+
+## collect licenses in licenses directory
+
+LICENSE_FILES=$(find licenses | grep -e LICENSE)
+
+for lic in $LICENSE_FILES; do
+    cat $lic >> LICENSES.txt
+done
+
+## collect licenses in intel/licenses directory
+
+INTEL_LICENSE_FILES=$(find intel/licenses | grep -e LICENSE)
+
+for lic in $INTEL_LICENSE_FILES; do
+    cat $lic >> LICENSES.txt
+done
